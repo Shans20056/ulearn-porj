3acfabc9ba2ad71f5576799c08213d70bc4ad4ea
Shans20056 <egorshab3@gmail.com>
2025-05-21 23:00:54 +0500

    Настройка отбражения шаблона

--- ulearnProject/main/views.py
+++ ulearnProject/main/views.py
@@ -4,3 +4,18 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'article_list.html', {'articles': articles})
+
+def home(request):
+    return render(request, 'main/home.html', {
+        'title': 'Главная страница'
+    })
+
+def about(request):
+    return render(request, 'main/about.html', {
+        'title': 'О нас'
+    })
+
+def contact(request):
+    return render(request, 'main/contact.html', {
+        'title': 'Контакты'
+    })
